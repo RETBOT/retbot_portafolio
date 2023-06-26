09379a0dfc902bea2be6a697f40fd872fb37639f
RETBOT <robertoesquiveltr16@gmail.com>
2023-06-26 17:48:11 -0600

    Telefono udpate

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,36 @@
 import React from "react";
-
+import Tilt from "react-tilt";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { fadeIn } from "../utils/motion";
+
+const TechCard = ({
+  index,
+  icon,
+}) => {
+  return (
+    <div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-full sm:w-[360px] w-full '
+      >
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={icon}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
 
 const Tech = (esTelefono) => {
   return (
@@ -17,15 +45,11 @@ const Tech = (esTelefono) => {
             ))
           )
           : (
-            technologies.map((technology) => (
-              <div className='w-40 h-40' key={technology.name}>
-                <img class="rounded-full w-full h-full bg-white" src={technology.icon} />
-              </div>
+            technologies.map((technology, index) => (
+              <TechCard key={`project-${index}`} index={index} {...technology} />
             ))
           )
       }
-
-
     </div>
   );
 };
