fcab982fc1c514b3fed2659e760549c7953a7d97
RETBOT <robertoesquiveltr16@gmail.com>
2023-06-28 15:26:18 -0600

    add server

---
+++ client/src/constants/env_mail.js
@@ -0,0 +1,6 @@
+// keys en https://www.emailjs.com/
+export const ENVMAIL = {
+    VITE_APP_EMAILJS_SERVICE_ID: "Key", // Add new Service
+    VITE_APP_EMAILJS_TEMPLATE_ID: "Key", // Add new Templates 
+    VITE_APP_EMAILJS_PUBLIC_KEY: "key", // key profile
+};
--- src/constants/env_mail.js
+++
@@ -1,6 +0,0 @@
-// keys en https://www.emailjs.com/
-export const ENVMAIL = {
-    VITE_APP_EMAILJS_SERVICE_ID: "KEY", // Add new Service
-    VITE_APP_EMAILJS_TEMPLATE_ID: "KEY", // Add new Templates 
-    VITE_APP_EMAILJS_PUBLIC_KEY: "KEY", // key profile
-};
